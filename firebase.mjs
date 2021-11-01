6bb4725e1aa2742871583efd37e7e8df4f7aefb5
djaivon <jaivon_daniels@bloomfield.edu>
2021-11-01 10:52:08 -0400

    New Changes

---
+++ firebase.mjs
@@ -0,0 +1,57 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+  
+  import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+  
+  import {getFirestore, getDocs, collection, addDoc} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB6s3_yaGuBqGCdsjGOXfd_lnq2meWGa7k",
+    authDomain: "dogs-2770f.firebaseapp.com",
+    projectId: "dogs-2770f",
+    storageBucket: "dogs-2770f.appspot.com",
+    messagingSenderId: "255271722509",
+    appId: "1:255271722509:web:bfe289f0da865cd5eebf44"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const db = getFirestore();
+
+
+  window.isloggedIn = function(){
+        return auth.currentUser == null;
+  }
+
+  window.login = function(email,password){
+       return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  window.signup = function(email,password){
+        return createUserWithEmailAndPassword(auth, email, password);
+}
+
+window.logout = function(){
+    auth.signOut();
+}
+
+window.onLogin = function( f ){
+    onAuthStateChanged(user, user => {
+        f(user);
+    });
+}
+
+window.addComment = function(comment){
+    return addDoc(collection(db, "comments"), {comment} );
+}
+
+window.forEachComment = function( f ){
+    var docs = await getDocs( collection(db, "comments") );
+    console.log(docs);
+    docs.forEach( doc => f(doc.data()) );
+}
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const $ = document.querySelector.bind(document);
 
 function showImage(){
@@ -29,6 +31,57 @@ this.fetch('https://dog.ceo/api/breeds/list/all')
     Object.keys(data.message)
     .forEach(MakeButton) 
 });
+
+onLogin(user => {
+    if(user){
+        //user just logged in
+        $('#addCommentDiv').style.display = 'block';
+        $('#loginDiv').style.display = 'none';
+        $('#signupDiv').style.display = 'none';
+    }else{
+        //user just logged out
+        $('#addCommentDiv').style.display = 'none';
+        $('#loginDiv').style.display = 'block';
+    }
+});
+
+if(isLoggedIn()){
+    $('#addCommentDiv').style.display = 'block';
+}else{
+    $('#loginDiv').style.display = 'block';
+}
+
+$('#loginLink').onclick = function(){
+    $('#loginDiv').style.display = 'block';
+    $('signupDiv').style.display = 'none';
+}
+
+$('#signupLink').onclick = function(){
+    $('#loginDiv')
+}
+
+$('#logout').onclick = function(){
+    logout();
+}
+
+$('#loginBtn').onClick = function(){
+    login( $('#email').value, $('#password').value)
+    .catch( err => $('.error').innerText = err.message);
+}
+
+$('#registerBtn').onClick = function(){
+    signup( $('#emailReg').value, $('#passwordReg').value)
+    .catch( err => $('.error').innerText = err.message);
+}
+
+$('#addCommentBtn').onclick = function(){
+    addComment( $('#newComment').value )
+    .then( () => {
+        createComment({comment: $('#newComment').value});
+        $('#newComment').value = '';
+    })
+    .catch( err => $('.error').innerText = err.message )
+}
 
 }
 
